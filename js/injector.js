64104fab366cbc49d9e6b9135a71aaeb458af0b8
James <official.chubbyroo@gmail.com>
2019-10-14 02:33:02 -0700

    update to injector
    bumped version number

--- js/injector.js
+++ js/injector.js
@@ -1,15 +1,36 @@
-let href = window.location.href;
 
-pen("<script>").attr("src", chrome.runtime.getURL("js/pen.js")).appendTo(pBody);
-pen("<script>").attr("src", chrome.runtime.getURL("js/addons.js")).appendTo(pBody);
 
-switch (true) {
-  case href.includes('msg/others/'):
-    pen("<script>").attr("src", chrome.runtime.getURL('js/notif-sorter.js'))
-    .appendTo(pBody);
-  break;
+(function () {
+  // this stuff at the top just makes it easier on me-
+  // cause I hate raw js but its essential sometimes if you don't want to take up resources
+  // and why I'm doing "window" everytime, is because I want it to be specific and not conflict
 
-  default:
-    console.log('no scripts to inject');
-   // a default
-}
+  let href = window.location.href,
+  gurl = (x) => chrome.runtime.getURL(x),
+  cel = (x) => window.document.createElement(x),
+  body = window.document.body,
+  head = window.document.head,
+  p; // general insertion variable or GIV
+
+  (function () {
+    p = cel('script');
+    p.setAttribute('src', gurl('js/pen.js'));
+    body.appendChild(p);
+
+    p = cel('script');
+    p.setAttribute('src', gurl('js/addons.js'));
+    body.appendChild(p);
+  }());
+
+  switch (true) {
+    case href.includes('msg/others/'):
+      p = cel('script');
+      p.setAttribute('src', gurl('js/notif-sorter.js'));
+      body.appendChild(p);
+    break;
+
+    default:
+      console.log('no scripts to inject');
+      // a default
+  }
+}());
